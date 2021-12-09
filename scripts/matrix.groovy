cf5c2a89f7566b4e7f80b03d5156da322293e1c3
aiobjects <90204773+aiobjects@users.noreply.github.com>
2021-12-09 12:19:32 +0530

    Update matrix.groovy

--- scripts/matrix.groovy
+++ scripts/matrix.groovy
@@ -1,8 +1,14 @@
 job('demo') {
-    
+   
     
     authorization {
         permission('hudson.model.Item.Workspace:authenticated')
+        permission('hudson.model.Item.Discover', 'anonymous')
+        permissions('myUserOrGroup', [
+                'hudson.model.Item.Build',
+                'hudson.model.Item.Discover',
+                'hudson.model.Item.Cancel'
+        ])
     }
 
     steps {
